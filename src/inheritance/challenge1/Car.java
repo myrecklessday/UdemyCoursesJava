c52d23fd2ba0a6800c89fea11095539a7df9b2c7
myrecklessday <sobolhellen@gmail.com>
2019-05-22 23:21:04 +0200

    New challenges for inheritance topic.

---
+++ src/inheritance/challenge1/Car.java
@@ -0,0 +1,34 @@
+package inheritance.challenge1;
+
+public class Car extends Vehicle {
+
+    private int wheels;
+    private int doors;
+    private boolean isManual;
+    private int gears;
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, boolean isManual, int gears) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.isManual = isManual;
+        this.gears = gears;
+        this.currentGear = 1;
+    }
+
+    public int getCurrentGear(){
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear){
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction){
+        System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+        move(speed, direction);
+    }
+
+}
---
+++ src/inheritance/challenge1/Main.java
@@ -0,0 +1,11 @@
+package inheritance.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        Jaguar jaguar = new Jaguar(24);
+        jaguar.steer(55);
+        jaguar.accelerate(30);
+        jaguar.accelerate(10);
+        jaguar.accelerate(-45);
+    }
+}
