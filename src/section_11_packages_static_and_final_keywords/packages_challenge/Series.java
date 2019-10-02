ca34805268f39ed544bf482e58deb164128387ba
myrecklessday <sobolhellen@gmail.com>
2019-10-02 23:28:18 +0200

    Section 11 - Packages challenge

---
+++ src/section_11_packages_static_and_final_keywords/packages_challenge/Series.java
@@ -0,0 +1,49 @@
+package section_11_packages_static_and_final_keywords.packages_challenge;
+
+public class Series {
+
+    public static long nSum(int n) {
+
+        return (n * (n + 1)) / 2;
+
+    }
+
+    public static long factorial(int n) {
+        int f = 1;
+
+//        while (n > 0 ) {
+//            f *= n;
+//            n = n - 1;
+//        }
+
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+
+        return f;
+    }
+
+    public static long fibonacci(int n) {
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        long nMinus1 = 1;
+        long nMinus2 = 0;
+        long fib = 0;
+
+        for (int i = 1; i < n; i++) {
+            fib = (nMinus2 + nMinus1);
+            nMinus2 = nMinus1;
+            nMinus1 = fib;
+        }
+
+        return fib;
+
+    }
+
+
+}
